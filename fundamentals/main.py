e19c7cc9c08574d79169fbd1a523c2d0c6dbc580
Juanse <juancgomdotpad@gmail.com>
2022-01-25 19:06:50 -0500

    validations_query

--- fundamentals/main.py
+++ fundamentals/main.py
@@ -6,7 +6,7 @@ from typing import Optional
 from pydantic import BaseModel
 
 from fastapi import FastAPI
-from fastapi import Body
+from fastapi import Body, Query
 
 app = FastAPI()
 
@@ -29,3 +29,12 @@ def home():
 @app.post('/person/new')
 def create_person(person: Person = Body(...)):
     return person
+
+# Validations: Query Parameters
+
+@app.get('/person/detail')
+def show_person(
+    name: Optional[str] = Query(None, min_length=1, max_length=50),
+    age: Optional[int] = Query(None, ge=0, le=150)
+):
+    pass
