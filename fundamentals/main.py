a8c8102ed5c8f1800f3ceaa91d75236dc4e8d24f
Juanse <juancgomdotpad@gmail.com>
2022-01-26 20:30:23 -0500

    validations_body

--- fundamentals/main.py
+++ fundamentals/main.py
@@ -12,6 +12,11 @@ app = FastAPI()
 
 # Models
 
+class Location(BaseModel):
+    city: str
+    state: str
+    country: str
+
 class Person(BaseModel):
     first_name: str 
     last_name: str
@@ -62,3 +67,22 @@ def show_person(
         )
     ):
     return {person_id: "It exists!"}
+
+
+#Validations: Request Body
+
+@app.put('/person/{person_id}')
+def update_person(
+    person_id: int = Path(
+        ...,
+        title='Person Id',
+        description="This is the person id.",
+        ge=1
+    ),
+    person: Person = Body(...),
+    location: Location = Body(...)
+):
+    results = person.dict()
+    results.update(location.dict())
+
+    return results
