7bfa075674d565c248f81627404d40402fa16104
Juanse <juancgomdotpad@gmail.com>
2022-02-10 19:36:46 -0500

    models_users

--- twitter-api-fastapi/main.py
+++ twitter-api-fastapi/main.py
@@ -1,6 +1,6 @@
 # Python
 from uuid import UUID
-from datetime import date
+from datetime import date,datetime
 from typing import Optional
 
 # Pydantic
@@ -17,12 +17,12 @@ app = FastAPI()
 class UserBase(BaseModel):
     user_id: UUID = Field(...)
     email: EmailStr = Field(...)
-    pass
 
 class UserLogin(UserBase):
     password: str = Field(
         ...,
-        min_length=8
+        min_length=8,
+        max_length=64
     )
 
 class User(UserBase):
@@ -34,7 +34,15 @@ class User(UserBase):
     birth_date: Optional[date] = Field(default=None)
 
 class Tweet():
-    pass
+    tweet_id: UUID = Field(...)
+    content: str = Field(
+        ...,
+        max_length=1,
+        max_length=256
+    )
+    created_at: datetime = Field(default=datetime.now())
+    updated_at: Optional[datetime] = Field(default=None)
+    by: User = Field(...)
 
 @app.get(path='/')
 def home():
